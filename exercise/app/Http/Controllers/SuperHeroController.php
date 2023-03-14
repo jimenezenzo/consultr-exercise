9b7bcfe34ae5f79a7784e0ec458ca11a339491dd
Enzo Jimenez <enzo-jimenez@hotmail.com>
2023-03-14 11:37:05 -0300

    feat: add package Maatwebsite\Excel

--- exercise/app/Http/Controllers/SuperHeroController.php
+++ exercise/app/Http/Controllers/SuperHeroController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class SuperHeroController extends Controller
 {
     public function importCsv(Request $request)
     {
-
+        return response()->json([
+            'message' => 'successfully created'
+        ], Response::HTTP_CREATED);
     }
 }
